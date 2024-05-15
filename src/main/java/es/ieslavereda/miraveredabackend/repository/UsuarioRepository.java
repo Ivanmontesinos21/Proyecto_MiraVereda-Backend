991ba6aba6d49646d3b5df713a49b92d3bd98ac3
david <davidevis1266@gmail.com>
2024-05-15 19:26:46 +0200

    parte de pelicula

--- src/main/java/es/ieslavereda/miraveredabackend/repository/UsuarioRepository.java
+++ src/main/java/es/ieslavereda/miraveredabackend/repository/UsuarioRepository.java
@@ -33,24 +33,56 @@ public class UsuarioRepository implements IUsuarioRepository {
 
     @Override
     public Usuario addUsuario(Usuario usuario) throws SQLException {
-        return null;
+        String sql="Insert into Cliente (id_cliente, nombre, apellido,password,email,domicilio,codigopostal,tarjeta,fechanacimiento) values (?,?,?,?,?,?,?,?,?)";
+        try (Connection connection = dataSource.getConnection();
+        CallableStatement callableStatement = connection.prepareCall(sql)) {
+            callableStatement.setInt(1, usuario.getId());
+            callableStatement.setString(2, usuario.getNombre());
+            callableStatement.setString(3, usuario.getApellidos());
+            callableStatement.setString(4, usuario.getPassword());
+            callableStatement.setString(5, usuario.getEmail());
+            callableStatement.setString(6, usuario.getDomicilio());
+            callableStatement.setString(7, usuario.getCodigopostal());
+            callableStatement.setObject(8, usuario.getTarjeta());
+            callableStatement.setDate(9, (Date) usuario.getFechaNacimiento());
+            callableStatement.execute();
+        }
+        return usuario;
     }
 
     @Override
     public boolean updateUsuario(Usuario usuario) throws SQLException {
+        String sql="Update Cliente SET nombre=?,apellidos=?,password=?,email=?,domicilio=?,codigopostal=?,tarjeta=?,fechanacimiento=? WHERE id_cliente =" + usuario.getId();
+        try (Connection connection = dataSource.getConnection();
+        CallableStatement callableStatement = connection.prepareCall(sql)) {
+            callableStatement.setString(1, usuario.getNombre());
+            callableStatement.setString(2, usuario.getApellidos());
+            callableStatement.setString(3, usuario.getPassword());
+            callableStatement.setString(4, usuario.getEmail());
+            callableStatement.setString(5, usuario.getDomicilio());
+            callableStatement.setString(6, usuario.getCodigopostal());
+            callableStatement.setObject(7, usuario.getTarjeta());
+            callableStatement.setDate(8, (Date) usuario.getFechaNacimiento());
+            callableStatement.executeUpdate();
+        }
         return false;
     }
 
     @Override
     public Usuario deleteUsuario(int id) throws SQLException {
+        String sql = "DELETE FROM Cliente WHERE id_cliente = " + id;
         Usuario usuario = getUsuario(id);
-
+        try (Connection connection = dataSource.getConnection();
+        CallableStatement callableStatement = connection.prepareCall(sql)) {
+            callableStatement.execute();
+        }
+        return usuario;
     }
 
     @Override
     public List<Usuario> getAllUsuarios() throws SQLException {
         List<Usuario> usuarios = new ArrayList<>();
-        String sql = "SELECT * FROM usuario";
+        String sql = "SELECT * FROM Cliente";
         try (Connection con = dataSource.getConnection();
         PreparedStatement ps = con.prepareStatement(sql);
         ResultSet rs = ps.executeQuery()){
