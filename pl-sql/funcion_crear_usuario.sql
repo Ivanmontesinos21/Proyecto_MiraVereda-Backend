344f839097997797156db113371d39023e9c686b
JaimeMarti <152516293+JaimeMarti@users.noreply.github.com>
2024-05-20 02:14:20 +0200

    Añadir RETURN a funcion_crear_usuario.sql
    El booleano devuelto indica si el email esta libre (true) o en uso (false)

--- pl-sql/funcion_crear_usuario.sql
+++ pl-sql/funcion_crear_usuario.sql
@@ -5,29 +5,35 @@ CREATE OR REPLACE FUNCTION crear_usuario (
     email_input VARCHAR2,
     domicilio_input VARCHAR2,
     codigo_postal_input VARCHAR2,
-    fecha_nacimiento_input DATE
+    fecha_nacimiento_input DATE,
+    clientes OUT SYS_REFCURSOR
 )
-RETURN SYS_REFCURSOR
+RETURN BOOLEAN
 IS
 BEGIN
     DECLARE
-        cliente_cursor SYS_REFCURSOR;
         nueva_id_cliente INTEGER;
+        clientes_con_email INTEGER;
     BEGIN
-        nueva_id_cliente := cliente_sequence.nextval;
-        INSERT INTO CLIENTE VALUES (
-            nueva_id_cliente,
-            nombre_input,
-            apellidos_input,
-            contrasenya_input,
-            email_input,
-            domicilio_input,
-            codigo_postal_input,
-            fecha_nacimiento_input,
-            (SELECT CURRENT_TIMESTAMP FROM DUAL)
-        );
-        COMMIT;
-        OPEN cliente_cursor FOR SELECT * FROM CLIENTE WHERE id_cliente = nueva_id_cliente;
-        RETURN cliente_cursor;
+        SELECT COUNT(*) INTO clientes_con_email FROM CLIENTE WHERE email = email_input;
+        IF clientes_con_email = 0 THEN
+	        nueva_id_cliente := cliente_sequence.nextval;
+	        INSERT INTO CLIENTE VALUES (
+	            nueva_id_cliente,
+	            nombre_input,
+	            apellidos_input,
+	            contrasenya_input,
+	            email_input,
+	            domicilio_input,
+	            codigo_postal_input,
+	            fecha_nacimiento_input,
+	            (SELECT CURRENT_TIMESTAMP FROM DUAL)
+	        );
+	        COMMIT;
+	        OPEN clientes FOR SELECT * FROM CLIENTE WHERE id_cliente = nueva_id_cliente;
+	        RETURN TRUE;
+	    ELSE
+	    	RETURN FALSE;
+	    END IF;
     END;
 END;
