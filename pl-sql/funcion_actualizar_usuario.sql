9cdcfa647c491afaa0ff648886aa973a54d00503
JaimeMarti <152516293+JaimeMarti@users.noreply.github.com>
2024-05-20 02:37:21 +0200

    Create funcion_actualizar_usuario.sql

---
+++ pl-sql/funcion_actualizar_usuario.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION actualizar_usuario (
+    id_input INTEGER,
+    nombre_input VARCHAR2,
+    apellidos_input VARCHAR2,
+    contrasenya_input VARCHAR2,
+    email_input VARCHAR2,
+    domicilio_input VARCHAR2,
+    codigo_postal_input VARCHAR2,
+    fecha_nacimiento_input DATE
+)
+RETURN BOOLEAN
+IS
+BEGIN
+    DECLARE
+        clientes_con_email INTEGER;
+    BEGIN
+        SELECT COUNT(*) INTO clientes_con_email FROM CLIENTE WHERE email = email_input AND id_cliente <> id_input;
+        IF clientes_con_email = 0 THEN
+            UPDATE CLIENTE SET
+                nombre = nombre_input,
+                apellidos = apellidos_input,
+                contrasenya = contrasenya_input,
+                email = email_input,
+                domicilio = domicilio_input,
+                codigo_postal = codigo_postal_input,
+                fecha_nacimiento = fecha_nacimiento_input
+                WHERE id_cliente = id_input;
+            RETURN TRUE;
+        ELSE
+            RETURN FALSE;
+        END IF;
+    END;
+END;
