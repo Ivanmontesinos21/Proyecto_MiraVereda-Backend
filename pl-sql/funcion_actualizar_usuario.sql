6dd874e45d7357f8a16c0b8f94cfff4c039c3d20
JaimeMarti <152516293+JaimeMarti@users.noreply.github.com>
2024-05-21 20:30:05 +0200

    Update funcion_actualizar_usuario.sql

--- pl-sql/funcion_actualizar_usuario.sql
+++ pl-sql/funcion_actualizar_usuario.sql
@@ -16,15 +16,26 @@ BEGIN
     BEGIN
         SELECT COUNT(*) INTO clientes_con_email FROM CLIENTE WHERE email = email_input AND id_cliente <> id_input;
         IF clientes_con_email = 0 THEN
-            UPDATE CLIENTE SET
-                nombre = nombre_input,
-                apellidos = apellidos_input,
-                contrasenya = contrasenya_input,
-                email = email_input,
-                domicilio = domicilio_input,
-                codigo_postal = codigo_postal_input,
-                fecha_nacimiento = fecha_nacimiento_input
-                WHERE id_cliente = id_input;
+            IF contrasenya_input IS NULL THEN
+                UPDATE CLIENTE SET
+                    nombre = nombre_input,
+                    apellidos = apellidos_input,
+                    email = email_input,
+                    domicilio = domicilio_input,
+                    codigo_postal = codigo_postal_input,
+                    fecha_nacimiento = fecha_nacimiento_input
+                    WHERE id_cliente = id_input;
+            ELSE
+                UPDATE CLIENTE SET
+                    nombre = nombre_input,
+                    apellidos = apellidos_input,
+                    contrasenya = contrasenya_input,
+                    email = email_input,
+                    domicilio = domicilio_input,
+                    codigo_postal = codigo_postal_input,
+                    fecha_nacimiento = fecha_nacimiento_input
+                    WHERE id_cliente = id_input;
+            END IF;
             RETURN TRUE;
         ELSE
             RETURN FALSE;
